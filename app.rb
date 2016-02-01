7cc074d17b44d37b8934e623b4d46e0d130c76ba
Marion LV <marion.levisage@gmail.com>
2016-02-01 13:40:49 +0000

    Initialised shotgun, created new app pages

--- app.rb
+++ app.rb
@@ -1,1 +1,15 @@
 require 'sinatra'
+require 'shotgun'
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "Maaaaagic"
+end
+
+get '/meh' do
+  "Fuck this shit"
+end
